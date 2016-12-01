6f1fa444debd9a7a6ee2afea923475aaf5757ecf
Andreza Santos <andi.apsantos@gmail.com>
2016-12-01 14:19:33 -0200

    Update soul.s

--- soul.s
+++ soul.s
@@ -59,45 +59,45 @@ SET_GPIO:
 @@@@@@ TZIC @@@@@@
 SET_TZIC:
 
-    @ Constantes para os enderecos do TZIC
-    .set TZIC_BASE,             0x0FFFC000
-    .set TZIC_INTCTRL,          0x0
-    .set TZIC_INTSEC1,          0x84 
-    .set TZIC_ENSET1,           0x104
-    .set TZIC_PRIOMASK,         0xC
-    .set TZIC_PRIORITY9,        0x424
+	@ Constantes para os enderecos do TZIC
+	.set TZIC_BASE,             0x0FFFC000
+	.set TZIC_INTCTRL,          0x0
+	.set TZIC_INTSEC1,          0x84 
+	.set TZIC_ENSET1,           0x104
+	.set TZIC_PRIOMASK,         0xC
+	.set TZIC_PRIORITY9,        0x424
 
- 	@ Liga o controlador de interrupcoes
-    @ R1 <= TZIC_BASE
+	@ Liga o controlador de interrupcoes
+	@ R1 <= TZIC_BASE
 
-    ldr	r1, =TZIC_BASE
+	ldr	r1, =TZIC_BASE
 
-    @ Configura interrupcao 39 do GPT como nao segura
-    mov	r0, #(1 << 7)
-    str	r0, [r1, #TZIC_INTSEC1]
+	@ Configura interrupcao 39 do GPT como nao segura
+	mov	r0, #(1 << 7)
+	str	r0, [r1, #TZIC_INTSEC1]
 
-    @ Habilita interrupcao 39 (GPT)
-    @ reg1 bit 7 (gpt)
+	@ Habilita interrupcao 39 (GPT)
+	@ reg1 bit 7 (gpt)
 
-    mov	r0, #(1 << 7)
-    str	r0, [r1, #TZIC_ENSET1]
+	mov	r0, #(1 << 7)
+	str	r0, [r1, #TZIC_ENSET1]
 
-    @ Configure interrupt39 priority as 1
-    @ reg9, byte 3
+	@ Configure interrupt39 priority as 1
+	@ reg9, byte 3
 
-    ldr r0, [r1, #TZIC_PRIORITY9]
-    bic r0, r0, #0xFF000000
-    mov r2, #1
-    orr r0, r0, r2, lsl #24
-    str r0, [r1, #TZIC_PRIORITY9]
+	ldr r0, [r1, #TZIC_PRIORITY9]
+	bic r0, r0, #0xFF000000
+	mov r2, #1
+	orr r0, r0, r2, lsl #24
+	str r0, [r1, #TZIC_PRIORITY9]
 
-    @ Configure PRIOMASK as 0
-    eor r0, r0, r0
-    str r0, [r1, #TZIC_PRIOMASK]
+	@ Configure PRIOMASK as 0
+	eor r0, r0, r0
+	str r0, [r1, #TZIC_PRIOMASK]
 
-    @ Habilita o controlador de interrupcoes
-    mov	r0, #1
-    str	r0, [r1, #TZIC_INTCTRL]
+	@ Habilita o controlador de interrupcoes
+	mov	r0, #1
+	str	r0, [r1, #TZIC_INTCTRL]
 
 @@@@@@ SVC HANDLER @@@@@@
 
